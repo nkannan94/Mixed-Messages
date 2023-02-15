006430b2264f99effbd809ab51248bbf789659c4
nkannan94 <nadkann94@gmail.com>
2023-02-14 17:38:59 -0800

    Add objects containing words of power

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log('Bing Chilling and Big Chungus check check check');
-//Checking to see if files are being pushed to the repo
+//Objects containing arrays of the different words of power in each language
+const dragonWords = {
+    firstWord: ['Raan', 'Laas', 'Mid', 'Feim', 'Gol', 'Od', 'Hun', 'Lok', 'Ven', 'Zun', 'Faas', 'Mul', 'Joor', 'Gaan', 'Su', 'Yol', 'Fo', 'Iiz', 'Kaan', 'Krii', 'Tiid', 'Rii', 'Strun', 'Dur', 'Zul', 'Fus', 'Wuld'],
+    secondWord: ['Mir', 'Yah', 'Vur', 'Zii', 'Hah', 'Ah', 'Kaal', 'Vah', 'Gaar', 'Haal', 'Ru', 'Qah', 'Zah', 'Lah', 'Grah', 'Toor', 'Krah', 'Slen', 'Drem', 'Lun', 'Klo', 'Vaaz', 'Bah', 'Neh', 'Mey', 'Ro', 'Nah'],
+    thirdWord: ['Tah', 'Nir', 'Shaan', 'Gron', 'Dov', 'Viing', 'Zoor', 'Koor', 'Nos', 'Viik', 'Maar', 'Diiv', 'Frul', 'Haas', 'Dun', 'Shul', 'Diin', 'Nus', 'Ov', 'Aus', 'Ul', 'Zol', 'Qo', 'Viir', 'Gut', 'Dah', 'Kest']
+}
+
+const humanWords = {
+    firstWord: ['Animal', 'Life', 'Loyal', 'Fade', 'Earth', 'Snow', 'Hero', 'Sky', 'Wind', 'Weapon', 'Fear', 'Strength', 'Mortal', 'Stamina', 'Air', 'Fire', 'Frost', 'Ice', 'Kyne', 'Kill', 'Time', 'Essence', 'Storm', 'Curse', 'Voice', 'Force', 'Whirlwind'],
+    secondWord: ['Allegiance', 'Seek', 'Valor', 'Spirit', 'Mind', 'Hunter', 'Champion', 'Spring', 'Unleash', 'Hand', 'Run', 'Armor', 'Finite', 'Magicka', 'Battle', 'Inferno', 'Cold', 'Flesh', 'Peace', 'Leech', 'Sand', 'Tear', 'Wrath', 'Never', 'Fool', 'Balance', 'Fury'],
+    thirdWord: ['Pack', 'Hunt', 'Inspire', 'Bind', 'Dragon', 'Wing', 'Legend', 'Summer', 'Strike', 'Defeat', 'Terror', 'Wyrm', 'Temporary', 'Health', 'Grace', 'Sun', 'Freeze', 'Statue', 'Trust', 'Suffer', 'Eternity', 'Zombie', 'Lightning', 'Dying', 'Far', 'Push', 'Tempest']
+}
+
