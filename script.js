6695312d988a10422d4e6b13b130e05dfb4080ef
nkannan94 <nadkann94@gmail.com>
2023-02-14 17:48:21 -0800

    Outline the basic flow

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const dragonWords = {
     firstWord: ['Raan', 'Laas', 'Mid', 'Feim', 'Gol', 'Od', 'Hun', 'Lok', 'Ven', 'Zun', 'Faas', 'Mul', 'Joor', 'Gaan', 'Su', 'Yol', 'Fo', 'Iiz', 'Kaan', 'Krii', 'Tiid', 'Rii', 'Strun', 'Dur', 'Zul', 'Fus', 'Wuld'],
     secondWord: ['Mir', 'Yah', 'Vur', 'Zii', 'Hah', 'Ah', 'Kaal', 'Vah', 'Gaar', 'Haal', 'Ru', 'Qah', 'Zah', 'Lah', 'Grah', 'Toor', 'Krah', 'Slen', 'Drem', 'Lun', 'Klo', 'Vaaz', 'Bah', 'Neh', 'Mey', 'Ro', 'Nah'],
     thirdWord: ['Tah', 'Nir', 'Shaan', 'Gron', 'Dov', 'Viing', 'Zoor', 'Koor', 'Nos', 'Viik', 'Maar', 'Diiv', 'Frul', 'Haas', 'Dun', 'Shul', 'Diin', 'Nus', 'Ov', 'Aus', 'Ul', 'Zol', 'Qo', 'Viir', 'Gut', 'Dah', 'Kest']
-}
+};
 
 const humanWords = {
     firstWord: ['Animal', 'Life', 'Loyal', 'Fade', 'Earth', 'Snow', 'Hero', 'Sky', 'Wind', 'Weapon', 'Fear', 'Strength', 'Mortal', 'Stamina', 'Air', 'Fire', 'Frost', 'Ice', 'Kyne', 'Kill', 'Time', 'Essence', 'Storm', 'Curse', 'Voice', 'Force', 'Whirlwind'],
     secondWord: ['Allegiance', 'Seek', 'Valor', 'Spirit', 'Mind', 'Hunter', 'Champion', 'Spring', 'Unleash', 'Hand', 'Run', 'Armor', 'Finite', 'Magicka', 'Battle', 'Inferno', 'Cold', 'Flesh', 'Peace', 'Leech', 'Sand', 'Tear', 'Wrath', 'Never', 'Fool', 'Balance', 'Fury'],
     thirdWord: ['Pack', 'Hunt', 'Inspire', 'Bind', 'Dragon', 'Wing', 'Legend', 'Summer', 'Strike', 'Defeat', 'Terror', 'Wyrm', 'Temporary', 'Health', 'Grace', 'Sun', 'Freeze', 'Statue', 'Trust', 'Suffer', 'Eternity', 'Zombie', 'Lightning', 'Dying', 'Far', 'Push', 'Tempest']
-}
+};
 
+//Randomly generate the dragon word
+const numOfWords = dragonWords.firstWord.length -1; 
+let randomNum = Math.floor(Math.random() * numOfWords);
+console.log(numOfWords); //Should log length of array
+
+//Translate the dragon word to human language
+//The dragon word and human language indecies are equal
